e34992ca4c9274498f91ed698cc3de6c1bfa8960
sheldy <chikibamtioma@gmail.com>
2023-02-26 14:48:57 +0200

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_description():
 
 setuptools.setup(
     name="aiotubes",
-    version="2.0",
+    version="2.0.1",
     license='MIT',
     author="sheldy",
     description="Asynchronous Youtube API",
